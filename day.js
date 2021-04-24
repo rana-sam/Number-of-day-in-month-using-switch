c946a28e2298ba82e91319ada06a11a73a722bac
Muhammad Usama <82697108+rana-sam@users.noreply.github.com>
2021-04-24 12:43:17 +0500

    Done

---
+++ day.js
@@ -0,0 +1,43 @@
+let num=parseInt(prompt("Enter month to show days in that month"));
+switch (num) {
+    case 1:
+        console.log("There is 31 Days in this month");
+        break;
+    case 2:
+        console.log("There is 29 Days in this month");
+        break;
+    case 3:
+        console.log("There is 31 Days in this month");
+        break;
+    case 4:
+        console.log("There is 30 Days in this month");
+        break;
+    case 5:
+        console.log("There is 31 Days in this month");
+        break;
+    case 6:
+        console.log("There is 30 Days in this month");
+        break;
+    case 7:
+        console.log("There is 31 Days in this month");
+        break;
+    case 8:
+        console.log("There is 31 Days in this month");
+        break;
+    case 9:
+        console.log("There is 30 Days in this month");
+        break;
+    case 10:
+        console.log("There is 31 Days in this month");
+        break;
+    case 11:
+        console.log("There is 30 Days in this month");
+        break;
+    case 12:
+        console.log("There is 31 Days in this month");
+        break;
+
+    default:
+        console.log("Enter valid month");
+        break;
+}
